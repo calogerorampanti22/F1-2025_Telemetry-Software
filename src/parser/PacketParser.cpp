cc576fee0eeb280e200f9a985f4986444ce7341e
calogerorampanti22 <calogerorampanti7@gmail.com>
2026-06-26 14:09:53 +0000

    Added CarStatusPacket paring in Json

--- src/parser/PacketParser.cpp
+++ src/parser/PacketParser.cpp
@@ -47,57 +47,87 @@ std::string PacketParser::parsePacketToJson(const std::vector<uint8_t>& data) {
             const auto& myCar = telemetry.m_carTelemetryData[playerIndex];
         
             // Create JSON object
-            nlohmann::json j;
-            j["type"] = "telemetry";
-            j["speed"] = myCar.m_speed;
-            j["throttle"] = myCar.m_throttle;
-            j["steer"] = myCar.m_steer;
-            j["brake"] = myCar.m_brake;
-            j["clutch"] = myCar.m_clutch;
-            j["gear"] = myCar.m_gear;
-            j["engineRPM"] = myCar.m_engineRPM;
-            j["drs"] = myCar.m_drs;
-            j["revLightsPercent"] = myCar.m_revLightsPercent;
-            j["revLightsBitValue"] = myCar.m_revLightsBitValue;
-            j["tyresSurfaceTemperature"] = {myCar.m_tyresSurfaceTemperature[0],
-                                            myCar.m_tyresSurfaceTemperature[1],
-                                            myCar.m_tyresSurfaceTemperature[2],
-                                            myCar.m_tyresSurfaceTemperature[3]
-                                            };
-            j["tyresInnerTemperature"] = {myCar.m_tyresInnerTemperature[0],
-                                          myCar.m_tyresInnerTemperature[1],
-                                          myCar.m_tyresInnerTemperature[2],
-                                          myCar.m_tyresInnerTemperature[3]
-                                         };
-            j["tyresPressure"] = {myCar.m_tyresPressure[0],
-                                  myCar.m_tyresPressure[1],
-                                  myCar.m_tyresPressure[2],
-                                  myCar.m_tyresPressure[3]
-                                 };
-            j["surfaceType"] = {myCar.m_surfaceType[0],
-                                myCar.m_surfaceType[1],
-                                myCar.m_surfaceType[2],
-                                myCar.m_surfaceType[3]
-                               };
+            nlohmann::json telemetryJSONObject;
+            telemetryJSONObject["type"] = "telemetry";
+            telemetryJSONObject["speed"] = myCar.m_speed;
+            telemetryJSONObject["throttle"] = myCar.m_throttle;
+            telemetryJSONObject["steer"] = myCar.m_steer;
+            telemetryJSONObject["brake"] = myCar.m_brake;
+            telemetryJSONObject["clutch"] = myCar.m_clutch;
+            telemetryJSONObject["gear"] = myCar.m_gear;
+            telemetryJSONObject["engineRPM"] = myCar.m_engineRPM;
+            telemetryJSONObject["drs"] = myCar.m_drs;
+            telemetryJSONObject["revLightsPercent"] = myCar.m_revLightsPercent;
+            telemetryJSONObject["revLightsBitValue"] = myCar.m_revLightsBitValue;
+            telemetryJSONObject["tyresSurfaceTemperature"] = { 
+                                                               myCar.m_tyresSurfaceTemperature[0],
+                                                               myCar.m_tyresSurfaceTemperature[1],
+                                                               myCar.m_tyresSurfaceTemperature[2],
+                                                               myCar.m_tyresSurfaceTemperature[3]
+                                                             };
+            telemetryJSONObject["tyresInnerTemperature"] = { 
+                                                             myCar.m_tyresInnerTemperature[0],
+                                                             myCar.m_tyresInnerTemperature[1],
+                                                             myCar.m_tyresInnerTemperature[2],
+                                                             myCar.m_tyresInnerTemperature[3]
+                                                           };
+            telemetryJSONObject["tyresPressure"] = { 
+                                                     myCar.m_tyresPressure[0],
+                                                     myCar.m_tyresPressure[1],
+                                                     myCar.m_tyresPressure[2],
+                                                     myCar.m_tyresPressure[3]
+                                                   };
+            telemetryJSONObject["surfaceType"] = { 
+                                                   myCar.m_surfaceType[0],
+                                                   myCar.m_surfaceType[1],
+                                                   myCar.m_surfaceType[2],
+                                                   myCar.m_surfaceType[3]
+                                                 };
 
-            return j.dump();
+            return telemetryJSONObject.dump();
         }
         else {
             std::cerr << "[Car Telemetry Packet] ERROR: packet dimension not comform" << std::endl;
         }
     }
+
     else if(header.m_packetId == 7) { //ID 7 = Car Status Packet
         if(data.size() == sizeof(PacketCarStatusData)) {
             PacketCarStatusData status;
             std::memcpy(&status, data.data(), sizeof(PacketCarStatusData));
 
+            // The packet contains data of all 22 cars
+            // The header indicates which is our car index in these array
             uint8_t playerIndex = header.m_playerCarIndex;
-            
-            //Extracting tyres type and store it in a class variable
-            uint8_t tyreId = status.m_carStatusData[playerIndex].m_visualTyreCompound;
-            m_currentTyre = tyreId;
+            const auto& myCar = status.m_carStatusData[playerIndex];
 
-            return "";
+            // Create JSON Object
+            nlohmann::json statusJSONObject;
+            statusJSONObject["tractionControl"] = myCar.m_tractionControl;
+            statusJSONObject["antiLockBrakes"] = myCar.m_antiLockBrakes;
+            statusJSONObject["fuelMix"] = myCar.m_fuelMix;
+            statusJSONObject["frontBrakeBias"] = myCar.m_frontBrakeBias;
+            statusJSONObject["pitLimiterStatus"] = myCar.m_pitLimiterStatus;
+            statusJSONObject["fuelInTank"] = myCar.m_fuelInTank;
+            statusJSONObject["fuelCapacity"] = myCar.m_fuelCapacity;
+            statusJSONObject["fuelRemainingLaps"] = myCar.m_fuelRemainingLaps;
+            statusJSONObject["maxRPM"] = myCar.m_maxRPM;
+            statusJSONObject["idleRPM"] = myCar.m_idleRPM;
+            statusJSONObject["maxGears"] = myCar.m_maxGears;
+            statusJSONObject["drsAllowed"] = myCar.m_drsAllowed;
+            statusJSONObject["drsActivationDistance"] = myCar.m_drsActivationDistance;
+            statusJSONObject["actualTyreCompound"] = myCar.m_actualTyreCompound;
+            statusJSONObject["visualTyreCompound"] = myCar.m_visualTyreCompound;
+            statusJSONObject["tyresAgeLaps"] = myCar.m_tyresAgeLaps;
+            statusJSONObject["vehicleFiaFlags"] = myCar.m_vehicleFiaFlags;
+            statusJSONObject["enginePowerICE"] = myCar.m_enginePowerICE;
+            statusJSONObject["enginePowerMGUK"] = myCar.m_enginePowerMGUK;
+            statusJSONObject["ersStoreEnergy"] = myCar.m_ersStoreEnergy;
+            statusJSONObject["ersDeployMode"] = myCar.m_ersDeployMode;
+            statusJSONObject["ersDeployedThisLap"] = myCar.m_ersDeployedThisLap;
+            statusJSONObject["networkPaused"] = myCar.m_networkPaused;
+
+            return statusJSONObject.dump();
         }
         else {
             std::cerr << "[Car Status Packet] ERROR: packet dimension not comform" << std::endl;
